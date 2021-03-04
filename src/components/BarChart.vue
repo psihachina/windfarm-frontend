87d3b3894c41d8556bbec96eae42de96177be270
Nikita Podshivalov <psihachina@gmail.com>
2021-03-04 10:24:14 +0300

    Add bar chart for winds

---
+++ src/components/BarChart.vue
@@ -0,0 +1,23 @@
+ <script>
+import { Bar, mixins } from "vue-chartjs";
+const { reactiveProp } = mixins;
+export default {
+  name: "BarChart",
+  extends: Bar,
+  mixins: [reactiveProp],
+  props: ["chartData", "options"],
+  mounted() {
+    this.renderChart(this.chartData, this.options);
+  },
+  watch: {
+    options: function (newOptions, oldOption) {
+      console.log(newOptions, oldOption);
+      this._chart.destroy();
+      this.renderChart(this.chartData, {
+        responsive: true,
+        maintainAspectRadio: false,
+      });
+    },
+  },
+};
+</script>
