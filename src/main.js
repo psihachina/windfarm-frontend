334a07dc6dfce158683e8c0ce670fd0533ad9897
Nikita Podshivalov <psihachina@gmail.com>
2021-03-04 10:19:08 +0300

    Add apexcharts

--- src/main.js
+++ src/main.js
@@ -5,10 +5,14 @@ import store from './store/store'
 import vuetify from './plugins/vuetify';
 import Axios from 'axios'
 import x5GMaps from 'x5-gmaps'
+import VueApexCharts from 'vue-apexcharts'
 
-Vue.use(x5GMaps, { key: 'AIzaSyDTs3gjp3pl-TRUyYh_LOsTjI4CphPBYq4', libraries: ['places',"drawing"] })
+Vue.use(x5GMaps, { key: 'AIzaSyDTs3gjp3pl-TRUyYh_LOsTjI4CphPBYq4', libraries: ['places', "drawing"] })
+Vue.use(VueApexCharts)
+Vue.component('apexchart', VueApexCharts)
 
 Vue.prototype.$http = Axios;
+
 
 const token = localStorage.getItem('token')
 if (token) {
